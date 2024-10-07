29ce6ced140c8ef930571fa47763618caf5830c0
Sk Nazibul Hossain <sknazibul5986@gmail.com>
2024-10-07 18:08:37 +0530

    modify Auth0ProviderWithNavigate component

--- frontend/src/auth/Auth0ProviderWithNavigate.tsx
+++ frontend/src/auth/Auth0ProviderWithNavigate.tsx
@@ -11,6 +11,10 @@ const Auth0ProviderWithNavigate = ({ children }: Props) => {
     const domain = import.meta.env.VITE_AUTH0_DOMAIN;
     const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID
     const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL
+    console.log(domain);
+    console.log(clientId);
+    console.log(redirectUri);
+    
     
     const onRedirectCallback = (appState?: AppState, user?: User) => {
         navigate('/auth-callback')
