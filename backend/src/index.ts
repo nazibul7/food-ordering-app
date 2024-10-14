d010b6329fe6f613c16d484e63a611ef76e9abe3
Sk Nazibul Hossain <sknazibul5986@gmail.com>
2024-10-14 19:20:59 +0530

    setup to deploy in render

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,4 @@
-import express from "express"
+import express, { Request, Response } from "express"
 import cors from "cors"
 import "dotenv/config"
 import mongoose from "mongoose"
@@ -10,6 +10,9 @@ const app = express()
 // middlewares
 app.use(express.json())
 app.use(cors())
+app.get('/health',async(req:Request,res:Response)=>{
+    res.send("Health OK!")
+})
 app.use('/api/v1/user',userRoutes)
 
 // Global error handling middlewares
@@ -18,7 +21,7 @@ app.use(globalErrorHandlerMiddleware)
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string)
     .then(() => {
-        app.listen(process.env.PORT, () => {
+        app.listen(process.env.PORT || 3000, () => {
             console.log('Connected to MongoDB database');
             console.log(`Server is listening on port ${process.env.PORT}`);
         })
