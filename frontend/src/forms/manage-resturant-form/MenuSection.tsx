b0584518e5fea98e30e47dd94cbad24238d7afe6
Sk Nazibul Hossain <sknazibul5986@gmail.com>
2024-10-24 19:07:26 +0530

    remove unused import

--- frontend/src/forms/manage-resturant-form/MenuSection.tsx
+++ frontend/src/forms/manage-resturant-form/MenuSection.tsx
@@ -1,5 +1,5 @@
 import { Button } from "@/components/ui/button"
-import { FormDescription, FormField, FormItem, FormMessage } from "@/components/ui/form"
+import { FormDescription, FormField, FormItem } from "@/components/ui/form"
 import { useFieldArray, useFormContext } from "react-hook-form"
 import MenuItemInput from "./MenuItemInput"
 
