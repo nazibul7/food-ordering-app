932f0769b9ebeddebfb398f0a648bce9e76ed532
Sk Nazibul Hossain <sknazibul5986@gmail.com>
2024-11-06 21:46:15 +0530

    adjusted search bar input component css

--- frontend/src/components/SearchBar.tsx
+++ frontend/src/components/SearchBar.tsx
@@ -1,6 +1,6 @@
 import { zodResolver } from "@hookform/resolvers/zod"
 import { Search } from "lucide-react"
-import { useForm} from "react-hook-form"
+import { useForm } from "react-hook-form"
 import { z } from "zod"
 import { Form, FormControl, FormField, FormItem } from "./ui/form"
 import { Input } from "./ui/input"
@@ -23,28 +23,32 @@ export default function SearchBar({ onSubmit, placeHolder, onReset }: Props) {
             searchQuery: ''
         }
     })
-    
-    const handleReset=()=>{
+
+    const handleReset = () => {
         form.reset({
-            searchQuery:""
+            searchQuery: ""
         })
-        if(onReset){
+        if (onReset) {
             onReset()
         }
     }
     return (
         <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className={`flex items-center gap-3 justify-between border-2 rounded-full p-3 ${form.formState.errors.searchQuery && 'border-red-500'}`}>
-                <Search strokeWidth={2.5} size={30} className="ml-1 text-orange-500 hidden md:block" />
-                <FormField  control={form.control} name="searchQuery" render={({ field }) => {
-                    return <FormItem>
-                        <FormControl>
-                            <Input {...field} placeholder={placeHolder} className="border-none shadow-none focus-visible:ring-0" />
-                        </FormControl>
-                    </FormItem>
-                }} />
-                {form.formState.isDirty && <Button onClick={handleReset} type="button" variant="outline" className="rounded-full">Clear</Button>}
-                <Button type="submit" className="rounded-full bg-orange-500">Search</Button>
+            <form onSubmit={form.handleSubmit(onSubmit)} className={`flex items-center gap-3 border-2 rounded-full p-3 ${form.formState.errors.searchQuery && 'border-red-500'}`}>
+                <div className="flex items-center flex-1">
+                    <Search strokeWidth={2.5} size={30} className="ml-1 text-orange-500 hidden md:block" />
+                    <FormField control={form.control} name="searchQuery" render={({ field }) => {
+                        return <FormItem className="flex-1">
+                            <FormControl>
+                                <Input {...field} placeholder={placeHolder} className="border-none shadow-none focus-visible:ring-0" />
+                            </FormControl>
+                        </FormItem>
+                    }} />
+                </div>
+                <div className="flex gap-1">
+                    {form.formState.isDirty && <Button onClick={handleReset} type="button" variant="outline" className="rounded-full">Clear</Button>}
+                    <Button type="submit" className="rounded-full bg-orange-500">Search</Button>
+                </div>
             </form>
         </Form>
     )
