0735d00b2e71367f68c43d03191a6717840e567a
Sk Nazibul Hossain <sknazibul5986@gmail.com>
2024-11-08 22:27:01 +0530

    add query param in url in resturant-api-2 api

--- frontend/src/api/ResturantApi2.tsx
+++ frontend/src/api/ResturantApi2.tsx
@@ -1,13 +1,20 @@
+import { SearchState } from "@/pages/SearchPage";
 import { ResturantSearchResponse } from "@/types";
 import { useQuery } from "react-query";
 
 
+
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
-export const useSearchResturant = (city?: string) => {
-    const createSearchRequest = async ():Promise<ResturantSearchResponse> => {
+export const useSearchResturant = (searchState?: SearchState, city?: string) => {
+    const createSearchRequest = async (): Promise<ResturantSearchResponse> => {
         try {
-            const response = await fetch(`${API_BASE_URL}/api/resturant/search/${city}`, {
+            const params = new URLSearchParams()
+            params.append('searchQuery', searchState?.searchQuery as string)
+            params.append("selectedCusine", `${searchState?.selectedCusine.join(',')}`)
+            params.append('sortOptions', `${searchState?.sortOptions}`)
+            params.append('page', `${searchState?.page}`)
+            const response = await fetch(`${API_BASE_URL}/api/resturant/search/${city}?${params.toString()}`, {
                 method: "GET"
             })
             if (!response.ok) {
@@ -18,8 +25,8 @@ export const useSearchResturant = (city?: string) => {
             throw new Error(error as string)
         }
     }
-    const { data: results, isLoading } = useQuery('searchResturant', createSearchRequest, {
+    const { data: results, isLoading } = useQuery(['searchResturant',searchState], createSearchRequest, {
         enabled: !!city
     })
-    return {results,isLoading}
+    return { results, isLoading }
 }
