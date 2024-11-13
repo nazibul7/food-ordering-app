4c65b362752a0d5eae1929d8cb218674cd578241
Sk Nazibul Hossain <sknazibul5986@gmail.com>
2024-11-13 19:19:44 +0530

    add get resturnat by id api

--- frontend/src/api/ResturantApi2.tsx
+++ frontend/src/api/ResturantApi2.tsx
@@ -1,7 +1,6 @@
 import { SearchState } from "@/pages/SearchPage";
-import { ResturantSearchResponse } from "@/types";
+import { Resturant, ResturantSearchResponse } from "@/types";
 import { useQuery } from "react-query";
-
 
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
@@ -25,8 +24,27 @@ export const useSearchResturant = (searchState?: SearchState, city?: string) => 
             throw new Error(error as string)
         }
     }
-    const { data: results, isLoading } = useQuery(['searchResturant',searchState], createSearchRequest, {
+    const { data: results, isLoading } = useQuery(['searchResturant', searchState], createSearchRequest, {
         enabled: !!city
     })
     return { results, isLoading }
+}
+
+export const useGetResturantById = (resturantId?: string) => {
+    const getResturant = async (): Promise<Resturant> => {
+        try {
+            const response = await fetch(`${API_BASE_URL}/api/resturant/${resturantId}`, {
+                method: "GET"
+            })
+            if (!response.ok) {
+                throw new Error("Unable to get resturnat")
+            }
+            return response.json()
+        }
+        catch (error) {
+            throw new Error(error as string)
+        }
+    }
+    const { data: result, isLoading } = useQuery('getResturant', getResturant, { enabled: !!resturantId })
+    return { result, isLoading }
 }
