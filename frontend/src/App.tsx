e7ac27bc409f4194acaddefb7ea0ddad16d8c25d
root <root@soc-5CG2122DJ8.clients.intel.com>
2024-11-20 15:11:33 +0530

    add lazy loading to improve performance

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,27 +1,32 @@
 import { Navigate, Route, Routes } from 'react-router-dom'
-import Layout from './Layouts/Layout'
-import HomePage from './pages/HomePage'
-import AuthCallbackPage from './pages/AuthCallbackPage'
-import UserProfilePage from './pages/UserProfilePage'
-import ProtectedRoute from './auth/ProtectedRoute'
-import ManageResturantPage from './pages/ManageResturantPage'
-import SearchPage from './pages/SearchPage'
-import DetailsPage from './components/DetailsPage'
+import Layout from './Layouts/Layout';
+import ProtectedRoute from './auth/ProtectedRoute';
+import React, { Suspense } from 'react';
+
+const HomePage = React.lazy(() => import('./pages/HomePage'));
+const AuthCallbackPage = React.lazy(() => import('./pages/AuthCallbackPage'));
+const UserProfilePage = React.lazy(() => import('./pages/UserProfilePage'));
+const ManageResturantPage = React.lazy(() => import('./pages/ManageResturantPage'));
+const SearchPage = React.lazy(() => import('./pages/SearchPage'));
+const DetailsPage = React.lazy(() => import('./components/DetailsPage'));
 
 function App() {
   return (
-    <Routes>
-      <Route path='/' element={<Layout showHero><HomePage /></Layout>} />
-      <Route path='/auth-callback' element={<AuthCallbackPage />} />
-      <Route path='/search/:city' element={<Layout showHero={false}><SearchPage /></Layout>} />
-      <Route path='/detail/:resturantId' element={<Layout showHero={false}><DetailsPage /></Layout>} />
-      <Route element={<ProtectedRoute />}>
-        <Route path='/user-profile' element={<Layout showHero={false}><UserProfilePage /></Layout>} />
-        <Route path='/manage-resturant' element={<Layout showHero={false}><ManageResturantPage /></Layout>} />
-      </Route>
+    <Suspense fallback={<div>Loading...</div>}>
 
-      <Route path='*' element={<Navigate to={'/'} />} />
-    </Routes>
+      <Routes>
+        <Route path='/' element={<Layout showHero><HomePage /></Layout>} />
+        <Route path='/auth-callback' element={<AuthCallbackPage />} />
+        <Route path='/search/:city' element={<Layout showHero={false}><SearchPage /></Layout>} />
+        <Route path='/detail/:resturantId' element={<Layout showHero={false}><DetailsPage /></Layout>} />
+        <Route element={<ProtectedRoute />}>
+          <Route path='/user-profile' element={<Layout showHero={false}><UserProfilePage /></Layout>} />
+          <Route path='/manage-resturant' element={<Layout showHero={false}><ManageResturantPage /></Layout>} />
+        </Route>
+
+        <Route path='*' element={<Navigate to={'/'} />} />
+      </Routes>
+    </Suspense>
   )
 }
 export default App
