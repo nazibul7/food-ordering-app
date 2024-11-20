90fb25cb24f4b674b34f928b1253f68988f98461
root <root@soc-5CG2122DJ8.clients.intel.com>
2024-11-20 15:14:39 +0530

    add viteCompression for optimize compression in build

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,13 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
+import viteCompression from "vite-plugin-compression"
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteCompression({ algorithm: "gzip" })
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
