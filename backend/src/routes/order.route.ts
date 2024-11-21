c504400c681875f84055c45656e9972d88a3504c
Sk Nazibul Hossain <sknazibul5986@gmail.com>
2024-11-21 18:54:17 +0530

    add auth in get order route

--- backend/src/routes/order.route.ts
+++ backend/src/routes/order.route.ts
@@ -2,10 +2,10 @@ import express from "express"
 import { jwtCheck, jwtParse } from "../middleweres/auth"
 import { createCheckoutSession, orderStatus, stripeWebhookHandler } from "../controllers/order.controller"
 
-const router=express.Router()
+const router = express.Router()
 
-router.post('/checkout/create-checkout-session',jwtCheck,jwtParse,createCheckoutSession)
-router.post('/checkout/webhook',stripeWebhookHandler)
-router.get('/',orderStatus)
+router.post('/checkout/create-checkout-session', jwtCheck, jwtParse, createCheckoutSession)
+router.post('/checkout/webhook', stripeWebhookHandler)
+router.get('/', jwtCheck, jwtParse, orderStatus)
 
 export default router
