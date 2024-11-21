b522c1549d80e5f8a02633429576e959ae4b9458
root <root@soc-5CG2122DJ8.clients.intel.com>
2024-11-21 15:30:58 +0530

    add vite-plugin-imagemin for reduce imge file

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,11 +2,20 @@ import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 import viteCompression from "vite-plugin-compression"
+import viteImagemin from "vite-plugin-imagemin"
 
 export default defineConfig({
   plugins: [
     react(),
-    viteCompression({ algorithm: "gzip" })
+    viteCompression({ algorithm: "gzip" }),
+    viteImagemin({
+      pngquant:{
+        quality:[0.6,0.8]
+      },
+      webp:{
+        quality:75
+      }
+    })
   ],
   resolve: {
     alias: {
