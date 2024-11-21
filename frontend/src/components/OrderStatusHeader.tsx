b61aeb23800f2d9d8d48b47db1727ca1041c3870
Sk Nazibul Hossain <sknazibul5986@gmail.com>
2024-11-21 21:11:14 +0530

    add shadcn progress & dynamic order status

--- frontend/src/components/OrderStatusHeader.tsx
+++ frontend/src/components/OrderStatusHeader.tsx
@@ -1,4 +1,6 @@
 import { TOrderStatus } from '@/types'
+import { Progress } from './ui/progress'
+import { ORDER_STATUS } from '@/config/order-status-config'
 
 
 type TOrder = {
@@ -15,12 +17,18 @@ export default function OrderStatusHeader({ order }: TOrder) {
         const paddedMinutes = minutes > 10 ? minutes : `0${minutes}`
         return `${hours}:${paddedMinutes}`
     }
+    const getOrderInfo = () => {
+        const x = ORDER_STATUS.find(orderStatus => orderStatus.value == order.status) || ORDER_STATUS[0]
+        return x
+    }
+    getOrderInfo()
     return (
         <>
             <h1 className='text-4xl font-bold tracking-tighter flex flex-col gap-5 md:flex-row md:justify-between'>
-                <span>Order Status :{order.status}</span>
+                <span>Order Status : {getOrderInfo().label}</span>
                 <span>Expected by:{getExpectedDelivery()}</span>
             </h1>
+            <Progress value={getOrderInfo().progressValue} />
         </>
     )
 }
