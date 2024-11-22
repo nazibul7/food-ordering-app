efebbdbe827f1176097bba4910bf7781ab37026e
Sk Nazibul Hossain <sknazibul5986@gmail.com>
2024-11-22 20:01:10 +0530

    compelted order status page

--- frontend/src/pages/OrderStatusPages.tsx
+++ frontend/src/pages/OrderStatusPages.tsx
@@ -1,5 +1,7 @@
 import { useGetOrders } from '@/api/OrderApi'
+import OrderStatusDetail from '@/components/OrderStatusDetail'
 import OrderStatusHeader from '@/components/OrderStatusHeader'
+import { AspectRatio } from '@/components/ui/aspect-ratio'
 
 
 export default function OrderStatusPages() {
@@ -10,12 +12,18 @@ export default function OrderStatusPages() {
     if (!orders || orders.length == 0) {
         return "No orders found"
     }
-    
+
     return (
-        <div className=''>
-            {orders.map((order)=>(
-                <div className='bg-gray-50 p-10 rounded-lg' key={order._id}>
-                    <OrderStatusHeader order={order}/>
+        <div className='space-y-10'>
+            {orders.map((order) => (
+                <div className='bg-gray-50 space-y-10 p-10 rounded-lg' key={order._id}>
+                    <OrderStatusHeader order={order} />
+                    <div className='grid gap-10 md:grid-cols-2'>
+                        <OrderStatusDetail order={order} />
+                        <AspectRatio ratio={16 / 5}>
+                            <img src={order.resturant.imageUrl} alt="" className='rounded-md object-cover h-full w-full' />
+                        </AspectRatio>
+                    </div>
                 </div>
             ))}
         </div>
