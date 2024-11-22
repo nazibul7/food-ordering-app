f3984295767a004f1af5b84350f707d7e336248a
Sk Nazibul Hossain <sknazibul5986@gmail.com>
2024-11-22 20:02:14 +0530

    add OrderStatusDetail component for user to see

---
+++ frontend/src/components/OrderStatusDetail.tsx
@@ -0,0 +1,36 @@
+import { TOrderStatus } from "@/types"
+import { Separator } from "./ui/separator"
+
+type TOrderStatusDetails = {
+    order: TOrderStatus
+}
+
+export default function OrderStatusDetail({ order }: TOrderStatusDetails) {
+    
+    return (
+        <div className="space-y-5">
+            <div className="flex flex-col">
+                <span className="font-bold">Delivering to:</span>
+                <span>{order.deliveryDetails.name}</span>
+                <span>
+                    {order.deliveryDetails.addressLine1},{order.deliveryDetails.city}
+                </span>
+            </div>
+            <div className="flex flex-col">
+                <span className="font-bold">Your order</span>
+                <ul>
+                    {order.cartItems.map((item) => (
+                        <li key={item.menuItemId}>
+                            {item.name} x {item.quantity}
+                        </li>
+                    ))}
+                </ul>
+            </div>
+            <Separator />
+            <div className="flex flex-col">
+                <span className="font-bold">Total</span>
+                <span>${order.totalAmount}</span>
+            </div>
+        </div>
+    )
+}
