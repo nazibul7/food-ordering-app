01b561f79ac73fc7c233457425fed2382dcb53cf
Sk Nazibul Hossain <sknazibul5986@gmail.com>
2025-12-14 18:18:33 +0530

    add eslint in backend & frontend

---
+++ backend/eslint.config.js
@@ -0,0 +1,21 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  {
+    ignores: ["dist", "node_modules"],
+  },
+  {
+    files: ["src/**/*.ts"],
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.node,
+    },
+    rules: {
+      "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/no-explicit-any": "warn",
+    },
+  }
+);
