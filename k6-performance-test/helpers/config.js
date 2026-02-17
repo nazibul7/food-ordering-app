d714638e1b49dfce8a40f5e0f762b1c2dd6e6c46
Sk Nazibul Hossain <sknazibul5986@gmail.com>
2026-02-17 20:14:28 +0530

    add updated .env in config file

--- k6-performance-test/helpers/config.js
+++ k6-performance-test/helpers/config.js
@@ -1,3 +1,8 @@
 export const config = {
-    BASE_URL: __ENV.BASE_URL || 'http://localhost:3000'
+    API_BASE_URL: __ENV.API_BASE_URL || 'http://localhost:3000',
+    FRONTEND_URL: __ENV.FRONTEND_URL || 'http://localhost:5173',
+    AUTH0_DOMAIN: __ENV.AUTH0_DOMAIN,
+    AUTH0_CLIENT_ID: __ENV.AUTH0_CLIENT_ID,
+    AUTH0_CLIENT_SECRET: __ENV.AUTH0_CLIENT_SECRET,
+    AUTH0_AUDIENCE: __ENV.AUTH0_AUDIENCE,
 }
