3626a33075810e5168581bc273e619f678e07843
Sk Nazibul Hossain <sknazibul5986@gmail.com>
2026-05-10 20:28:09 +0530

    add metrics middleware, route & controller

---
+++ backend/src/controllers/metrics.controller.ts
@@ -0,0 +1,7 @@
+import { Request, Response } from "express";
+import client from "prom-client"
+
+export const getMetrics = async (req: Request, res: Response) => {
+    res.set("Content-Type", client.register.contentType)
+    res.send(await client.register.metrics())
+}
